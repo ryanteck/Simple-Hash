8ac09a9d3d533cb3c3ab53252dbef178e45af0ae
Ryan Walmsley <ryanteck@gmail.com>
2013-07-16 11:06:56 +0100

    added in some optimisations.

--- hash.py
+++ hash.py
@@ -2,6 +2,7 @@ import hashlib
 import time
 import sys
 arguments = sys.argv;
+total = []
 #print str(arguments);
 def hash(start):
 	start = start;
@@ -9,21 +10,25 @@ def hash(start):
 	current = start;
 	list = []
 
+
 	while(current<=stop):
 		hashme = str(current)
 		hashed = hashlib.sha512(hashme).hexdigest()
 		list.append(hashed)
 		current = current +1
 		
-	finalhash = str(list);
-	print hashlib.sha512(finalhash).hexdigest()
+		
+	
 
 timestart = time.time()
 arguments.pop(0);
 for argument in arguments:
 	#print argument;
 	hash(int(argument))
-	
-
+	finalnumlist = hashlib.sha512(str(list)).hexdigest();
+	total.append(finalnumlist);
+#print total;
+finalhash = str(total);
+print hashlib.sha512(finalhash).hexdigest()
 timeend =  time.time()
-print timeend-timestart
+#print timeend-timestart
